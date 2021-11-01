4b0f622c6b16930949a3bbbf3ebac605f4b453bc
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-01 17:11:56 +0100

    using int instead of char

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,11 +6,11 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 15:03:32 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/01 15:04:05 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/01 17:11:29 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isdigit(char c)
+int	ft_isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
 }
