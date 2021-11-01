eb6b6ee7630aee2c0722ccc6e0e8dd02696fb0a5
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-01 17:44:15 +0100

    added prototypes

--- libft.h
+++ libft.h
@@ -6,13 +6,20 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 11:53:13 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/01 12:24:33 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/01 17:43:34 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stdio.h>
+# include <stddef.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *str);
 
 #endif
