7a0a732e37fe2b5f73deb2e91ebc01323975b2c7
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-02 18:59:31 +0100

    added strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/02 12:31:13 by omoussao          #+#    #+#             */
+/*   Updated: 2021/11/02 18:56:24 by omoussao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	my_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	len;
+	size_t	i;
+
+	dest_len = my_strlen(dest);
+	src_len = my_strlen(src);
+	len = dest_len;
+	if (size <= dest_len)
+		return (size + src_len);
+	i = 0;
+	while (src[i] && len < size - 1)
+		dest[len++] = src[i++];
+	dest[len] = 0;
+	return (dest_len + src_len);
+}
+
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 11:53:13 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/02 12:19:27 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/02 18:51:18 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 #endif
