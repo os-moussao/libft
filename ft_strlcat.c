e1f6bd71bc06adb1acdfea90d5fb1067a0f7257e
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-02 19:25:26 +0100

    fixed norm

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 12:31:13 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/02 18:56:24 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/02 19:24:45 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,4 +40,3 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	dest[len] = 0;
 	return (dest_len + src_len);
 }
-
