ed309482289ed6b05d7a7ed19fdd234449e68fd1
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-03 20:11:17 +0100

    added ft_itoi

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 11:53:13 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/03 16:29:42 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/03 18:19:47 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *str, char c);
+char	*ft_itoa(int n);
 
 #endif
