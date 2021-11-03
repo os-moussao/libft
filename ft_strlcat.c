c2a7ce66cd2f494ad9020f49058d02e80a58a00a
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-03 15:29:45 +0100

    updated ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,21 +6,11 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 12:31:13 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/02 19:24:45 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/03 15:29:16 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t	my_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
@@ -29,8 +19,8 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	size_t	len;
 	size_t	i;
 
-	dest_len = my_strlen(dest);
-	src_len = my_strlen(src);
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
 	len = dest_len;
 	if (size <= dest_len)
 		return (size + src_len);
