1e730f69e9b51e80ad9f9600de8d13c65c944de9
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-04 17:23:35 +0100

    protection

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 19:29:21 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/01 19:38:35 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/04 17:03:56 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (!dest && !src)
+		return (NULL);
 	i = -1;
 	while (++i < n)
 		*(char *)(dest + i) = *(char *)(src + i);
