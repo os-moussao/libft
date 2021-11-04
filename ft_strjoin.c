687214d2c454f348abe993626fc6b202383081ee
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-04 19:12:49 +0100

    solved ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:17:51 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/03 15:37:46 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/04 18:51:04 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,22 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	size_t	len;
+	size_t	len1;
+	size_t	len2;
 
-	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str = malloc(len);
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	if (!s2 && s1)
+		return (ft_strdup(s1));
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = malloc(len1 + len2 + 1);
 	if (!str)
 		return (NULL);
-	ft_strlcat(str, s1, len);
-	ft_strlcat(str, s2, len);
+	ft_memmove(str, s1, len1);
+	ft_memmove(str + len1, s2, len2);
+	str[len1 + len2] = 0;
 	return (str);
 }
