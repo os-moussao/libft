e1c0c58963b75a12af130820f7f2d2b24044447e
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-04 13:20:03 +0100

    added ft_lstclear

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 11:53:13 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/04 13:06:48 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/04 13:16:08 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,6 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
 
 #endif
