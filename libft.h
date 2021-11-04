d34fff3a7c2f33f0ab61f0b179543263f4e52079
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-04 10:07:24 +0100

    added ft_lstadd_front

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 11:53:13 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/04 09:04:49 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/04 10:04:42 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,6 @@ void	ft_putstr_fd(char *str, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
