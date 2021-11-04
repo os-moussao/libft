6c53ca4acc8e02c44eaf552855e2eb4b3f133747
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-04 17:24:14 +0100

    more case handling

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 21:29:37 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/04 10:15:52 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/04 16:46:48 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,23 @@
 char	*ft_strnstr(const char *h, const char *n, size_t len)
 {
 	size_t	i;
+	size_t	j;
 
-	if (len == 0)
+	if (len == 0 && n[0])
 		return (NULL);
-	i = -1;
-	while (++i < len && n[i])
-		if (h[i] != n[i])
-			return (ft_strnstr(h + 1, n, len - 1));
-	if (!n[i])
+	if (!n[0])
 		return ((char *)h);
-	else
-		return (NULL);
+	i = -1;
+	while (++i < len && h[i])
+	{
+		j = -1;
+		while (n[++j] && i + j < len)
+		{
+			if (n[j] != h[i + j])
+				break ;
+		}
+		if (!n[j])
+			return ((char *)(h + i));
+	}
+	return (NULL);
 }
