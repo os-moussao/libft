1b248210eb8753fafce13a56647c67c05d4dccff
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-05 08:48:40 +0100

    fixed ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 15:44:14 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/04 19:55:45 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/05 08:47:34 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,19 @@ static int	in_set(char c, const char *set)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*str;
-	size_t	start;
-	size_t	end;
+	char		*str;
+	long long	start;
+	long long	end;
 
-	if (!s1)
+	if (!s1 || !set)
 		return (NULL);
-	if (!set || !*s1)
-		return (ft_strdup(s1));
 	end = ft_strlen(s1) - 1;
 	start = 0;
 	while (in_set(s1[start], set))
 		start++;
-	while (in_set(s1[end], set) && end)
+	while (in_set(s1[end], set) && end >= 0)
 		end--;
-	if (!end)
+	if (end < 0)
 		return (ft_strdup(""));
 	str = malloc(end - start + 2);
 	if (!str)
