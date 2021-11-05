3a8fa44442aa0ead8f660d718910c20534a2a701
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-05 08:48:18 +0100

    fixed ft_stjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:17:51 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/04 18:51:04 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/05 08:34:51 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len1;
 	size_t	len2;
 
-	if (!s1 && !s2)
-		return (ft_strdup(""));
-	if (!s2 && s1)
-		return (ft_strdup(s1));
-	if (!s1 && s2)
-		return (ft_strdup(s2));
+	if (!s1 || !s2)
+		return (NULL);
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
 	str = malloc(len1 + len2 + 1);
